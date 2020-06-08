93263003c0f98e6dab416d34f7c648d31abbbb83
Wendy Hsu <23854005.bg2303@gmai.com>
2020-06-08 13:41:19 +0800

    Updates

---
+++ precache-manifest.e7758bfd338007a4473573d2c6276370.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3936fd097c385055f5b6ba53d5fb77",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "24cb002b13740addccf0",
+    "url": "/my-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2e0ae2d37bdcb1db831c",
+    "url": "/my-app/static/js/2.4b9d3397.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.4b9d3397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24cb002b13740addccf0",
+    "url": "/my-app/static/js/main.23393b79.chunk.js"
+  },
+  {
+    "revision": "660024b2ea62aa0a0b7b",
+    "url": "/my-app/static/js/runtime-main.5119dbb0.js"
+  }
+]);
